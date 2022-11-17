91e3aa43a1bb876597004320ba29174bf1bb72cf
ahayat16 <amaury.hayat@gmail.com>
2022-11-17 14:15:36 +0100

    with_sp_guide_8

--- include/explicitaccel_microaccel/explicitaccel_microaccel_private.h
+++ include/explicitaccel_microaccel/explicitaccel_microaccel_private.h
@@ -7,9 +7,9 @@
 //
 // Code generated for Simulink model 'explicitaccel_microaccel'.
 //
-// Model version                  : 6.99
+// Model version                  : 6.98
 // Simulink Coder version         : 9.8 (R2022b) 13-May-2022
-// C/C++ source code generated on : Thu Nov 17 12:38:15 2022
+// C/C++ source code generated on : Thu Nov 17 12:23:17 2022
 //
 // Target selection: ert.tlc
 // Embedded hardware selection: Generic->Unspecified (assume 32-bit Generic)
